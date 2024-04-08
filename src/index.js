a89a951739869ad48fc559ee554fa6a6f30aa444
Randall <randall@inator.biz>
2024-04-08 16:16:19 -0700

    export modules needed for implementing external loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 export { Cast } from "./cast/index.js"
 export { ContractContextBuilder } from "./context/index.js"
+export { typeCheckFiles, typeCheckScripts } from "./compiler/ops.js"
+export { LoadedScriptsWriter } from "./codegen/LoadedScriptsWriter.js"
+export { loadCompilerLib } from "./compiler/ops.js"
 
 /**
  * @template {import("./cast/index.js").Cast} C
