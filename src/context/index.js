09eb44aaa42ff42b327a6024722f85568f8fb2d8
Christian Schmitz <cschmitz398@gmail.com>
2024-08-22 04:22:24 -0600

    export ContractContext type

--- src/context/index.js
+++ src/context/index.js
@@ -1,2 +1,8 @@
 export { ContractContextBuilder } from "./ContractContextBuilder.js"
 export { contractContextCache } from "./ContractContextCache.js"
+
+/**
+ * @template {{[name: string]: import("../codegen/index.js").LoadedValidator}} Vs
+ * @template {{[name: string]: import("../codegen/index.js").LoadedModule}} Ms
+ * @typedef {import("./ContractContext.js").ContractContext<Vs, Ms>} ContractContext
+ */
