9458d054633ec033370a1f9411c36a27d23c7bed
Christian Schmitz <cschmitz398@gmail.com>
2024-10-11 16:42:32 -0600

    LoadedModule and LoadedValidator exported because they are part of the ContractContextBuilder interface

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ export * from "./context/index.js"
 export { typeCheckFiles, typeCheckScripts } from "./compiler/ops.js"
 export { LoadedScriptsWriter } from "./codegen/LoadedScriptsWriter.js"
 export { loadCompilerLib } from "./compiler/ops.js"
+
+/**
+ * @typedef {import("./codegen/index.js").LoadedValidator} LoadedValidator
+ * @typedef {import("./codegen/index.js").LoadedModule} LoadedModule
+ */
