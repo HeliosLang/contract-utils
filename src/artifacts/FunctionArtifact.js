d065e3bfb63810e13dec01c65898d1f3a77a15fc
christianschmitz <cschmitz398@gmail.com>
2025-01-13 11:32:53 -0600

    FunctionArtifact exports $constData if the underlying constant can be evaluated without throwing an error

--- src/artifacts/FunctionArtifact.js
+++ src/artifacts/FunctionArtifact.js
@@ -1,11 +1,13 @@
+import { bytesToHex } from "@helios-lang/codec-utils"
+import { expectDefined, isRight } from "@helios-lang/type-utils"
 import {
-    genFuncType,
     genUserFuncArgsType,
     genUserFuncRetType
 } from "../codegen/LoadedScriptsWriter.js"
 import { ChildArtifactWriter } from "./ChildArtifactWriter.js"
 
 /**
+ * @import { UplcProgram } from "@helios-lang/uplc"
  * @import { UserFuncProps } from "../index.js"
  * @import { Artifact } from "./Artifact.js"
  * @import { FunctionDetails } from "./symbols.js"
@@ -24,7 +26,7 @@ export function writeFunctionArtifact(parent, name, details) {
 
     artifact.writeProgram("$program", program, true)
     artifact.writeProps(props)
-    artifact.writeEvals(props)
+    artifact.writeEvals(program, props)
 
     artifact.save()
 
@@ -49,13 +51,15 @@ export const $props = ${JSON.stringify(props, undefined, 4)}`)
     }
 
     /**
+     * @param {UplcProgram} program
      * @param {UserFuncProps} props
      */
-    writeEvals(props) {
+    writeEvals(program, props) {
         const safeArgsType = genUserFuncArgsType(props)
         const unsafeArgsType = genUserFuncArgsType(props, true)
         const retType = genUserFuncRetType(props)
         const unsafeRetType = genUserFuncRetType(props, true)
+        const isConst = safeArgsType == "{}"
 
         this.addImport("UplcLogger", "@helios-lang/uplc", true)
         this.addImport("UplcData", "@helios-lang/uplc", true)
@@ -63,28 +67,66 @@ export const $props = ${JSON.stringify(props, undefined, 4)}`)
 
         this.addImport("evalUserFunc", "@helios-lang/contract-utils")
             .writeDeclLine(
-                `export function $eval(args${safeArgsType == "{}" ? "?" : ""}: ${safeArgsType}, logOptions?: UplcLogger | undefined): ${retType}`
+                `export function $eval(args${isConst ? "?" : ""}: ${safeArgsType}, logOptions?: UplcLogger | undefined): ${retType}`
             )
             .writeDefLine(
-                `export function $eval(args${safeArgsType == "{}" ? " = {}" : ""}, logOptions = undefined) {
+                `export function $eval(args${isConst ? " = {}" : ""}, logOptions = undefined) {
     return evalUserFunc($program, $props, args, logOptions)
 }`
             )
             .addImport("evalUserFuncUnsafe", "@helios-lang/contract-utils")
             .writeDeclLine(
-                `export function $evalUnsafe(args${unsafeArgsType == "{}" ? "?" : ""}: ${unsafeArgsType}, logOptions?: UplcLogger | undefined): ${unsafeRetType}`
+                `export function $evalUnsafe(args${isConst ? "?" : ""}: ${unsafeArgsType}, logOptions?: UplcLogger | undefined): ${unsafeRetType}`
             )
             .writeDefLine(
-                `export function $evalUnsafe(args${unsafeArgsType == "{}" ? " = {}" : ""}, logOptions = undefined) {
+                `export function $evalUnsafe(args${isConst ? " = {}" : ""}, logOptions = undefined) {
     return evalUserFuncUnsafe($program, $props, args, logOptions)
 }`
             )
             .addImport("profileUserFunc", "@helios-lang/contract-utils")
             .writeDeclLine(
-                `export function $profile(args${unsafeArgsType == "{}" ? "?" : ""}: ${unsafeArgsType}, logOptions?: UplcLogger | undefined): CekResult`
+                `export function $profile(args${isConst ? "?" : ""}: ${unsafeArgsType}, logOptions?: UplcLogger | undefined): CekResult`
             )
-            .writeDefLine(`export function $profile(args${unsafeArgsType == "{}" ? " = {}" : ""}, logOptions = undefined) {
+            .writeDefLine(`export function $profile(args${isConst ? " = {}" : ""}, logOptions = undefined) {
     return profileUserFunc($program, $props, args, logOptions)
 }`)
+
+        if (isConst) {
+            // attempt to evaluate, and add $data
+            try {
+                const result = program.eval([])
+
+                if (
+                    isRight(result.result) &&
+                    typeof result.result.right != "string" &&
+                    result.result.right.kind == "data"
+                ) {
+                    const data = result.result.right.value
+
+                    const uplcDataType = expectDefined(
+                        {
+                            int: "IntData",
+                            bytes: "ByteArrayData",
+                            constr: "ConstrData",
+                            map: "MapData",
+                            list: "ListData"
+                        }[data.kind]
+                    )
+
+                    this.addImport(uplcDataType, "@helios-lang/uplc", true)
+                        .writeDeclLine(
+                            `export const $constData: ${uplcDataType}`
+                        )
+                        .addImport("decodeUplcData", "@helios-lang/uplc", false)
+                        .writeDefLine(
+                            `export const $constData = /* @__PURE__ */ decodeUplcData("${bytesToHex(data.toCbor())}")`
+                        )
+
+                    // TODO: $constValue
+                }
+            } catch (_e) {
+                // don't do anything, simply ignore
+            }
+        }
     }
 }
